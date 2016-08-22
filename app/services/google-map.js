371a80fc49f805634330fc0b75afc8fedf96a80f
August von Trapp and Patrick Lipscomb <august@>
2016-08-22 16:02:50 -0700

    add custom map styles

--- app/services/google-map.js
+++ app/services/google-map.js
@@ -4,27 +4,7 @@ export default Ember.Service.extend({
   googleMaps: window.google.maps,
   findMap(container, options) {
     var styledMapType = new this.googleMaps.StyledMapType(
-         [
-           {
-             stylers: [
-               { hue: '#cc6600' },
-               { saturation: -20 }
-             ]
-           },{
-             featureType: 'road',
-             elementType: 'geometry',
-             stylers: [
-               { lightness: 100 },
-               { visibility: 'simplified' }
-             ]
-           },{
-             featureType: 'road',
-             elementType: 'labels',
-             stylers: [
-               { visibility: 'off' }
-             ]
-           }
-         ],
+         [{"elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#0F0919"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#E4F7F7"}]},{"elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#002FA7"}]},{"featureType":"poi.attraction","elementType":"geometry.fill","stylers":[{"color":"#E60003"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#FBFCF4"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"color":"#FFED00"}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"color":"#D41C1D"}]},{"featureType":"poi.school","elementType":"geometry.fill","stylers":[{"color":"#BF0000"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"saturation":-100}]}],
          {name: 'Styled Map'});
     var map = new this.googleMaps.Map(container, options);
     map.mapTypes.set('styled_map', styledMapType);
